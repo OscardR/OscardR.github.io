f60e51ef6300194bcf1aca35cfcc72a45b4aa12e
Óscar Gómez <oscar.gomez.alcaniz@gmail.com>
2018-10-02 23:59:26 +0200

    Massive refactor - Every stylesheet in scss folder - Every javascript in js folder

--- gulpfile.js
+++ gulpfile.js
@@ -17,9 +17,9 @@ gulp.task('pug', () => {
 });
 
 gulp.task('sass', () =>
-    gulp.src('./cv/scss/**/*.scss')
+    gulp.src('./scss/**/*.scss')
         .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./cv/css'))
+        .pipe(gulp.dest('./css'))
 );
 
 gulp.task('default', gulp.parallel('sass', 'pug'));
