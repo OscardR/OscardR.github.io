b53a4af33989889ca97aea3828ff6d7427a8c0a3
Óscar Gómez <oscar.gomez.alcaniz@gmail.com>
2018-10-08 20:33:48 +0200

    Massive refactoring - Change package manager to Yarn - Add Gulp method for vendor JS and assets - Some changes to CV

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,31 @@ let gulp = require('gulp'),
     pug = require('gulp-jade'),
     sass = require('gulp-sass');
 
+const srcDir = './src',
+    jsDir = './js',
+    cssDir = './css',
+    vendorDir = './node_modules',
+    distDir = './';
+
+// Utility function to copy specific sub-folders to the dist folder
+let copy = (targetDir) =>
+    gulp.src(`${srcDir}/**/${targetDir}/*`)
+        .pipe(gulp.dest(distDir));
+
+// Copy images to images folders
+gulp.task('assets', () => copy('images'));
+
+
+gulp.task('vendor scripts', () =>
+    gulp.src([
+            `${vendorDir}/bootstrap/dist/js/bootstrap.js`,
+            `${vendorDir}//jquery/dist/jquery.js`
+        ]).pipe(gulp.dest(jsDir)));
+
+// Copy javascript to javascript folders
+gulp.task('scripts', () => copy('js'));
+
+// Compile Pug files
 gulp.task('pug', () => {
     let YOUR_LOCALS = {
         v: "2.0.0"
@@ -13,13 +38,15 @@ gulp.task('pug', () => {
         .pipe(pug({
             locals: YOUR_LOCALS
         }))
-        .pipe(gulp.dest('./'));
+        .pipe(gulp.dest(distDir));
 });
 
+// Compile SASS files
 gulp.task('sass', () =>
     gulp.src('./scss/**/*.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('./css'))
 );
 
-gulp.task('default', gulp.parallel('sass', 'pug'));
+// By default, do all
+gulp.task('default', gulp.parallel('assets', 'vendor scripts', 'scripts', 'sass', 'pug'));
