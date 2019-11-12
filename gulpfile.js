2f61a056d435f31e4039c445fbd3f183afe89311
Óscar Gómez Alcañiz <oscar.gomez.alcaniz@gmail.com>
2019-11-12 22:13:04 +0100

    Update jQuery and Bootstrap / Add HTTPS to links on CV / Small tweaks on colors for CV / Add minification for CSS and JS via Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,51 +1,55 @@
 "use strict";
 
 let gulp = require('gulp'),
-    pug = require('gulp-jade'),
-    sass = require('gulp-sass');
+  pug = require('gulp-jade'),
+  minify = require('gulp-minify'),
+  sass = require('gulp-sass');
 
 const srcDir = './src',
-    jsDir = './js',
-    cssDir = './',
-    vendorDir = './node_modules',
-    distDir = './';
+  jsDir = './js',
+  cssDir = './',
+  vendorDir = './node_modules',
+  distDir = './';
 
 // Utility function to copy specific sub-folders to the dist folder
 let copy = (targetDir) =>
-    gulp.src(`${srcDir}/**/${targetDir}/*`)
-        .pipe(gulp.dest(distDir));
+  gulp.src(`${srcDir}/**/${targetDir}/*`)
+    .pipe(gulp.dest(distDir));
 
 // Copy images to images folders
 gulp.task('assets', () => copy('images'));
 
 
 gulp.task('vendor scripts', () =>
-    gulp.src([
-        `${vendorDir}/bootstrap/dist/js/bootstrap.js`,
-        `${vendorDir}//jquery/dist/jquery.js`
-    ]).pipe(gulp.dest(jsDir)));
+  gulp.src([
+    `${vendorDir}/bootstrap/dist/js/bootstrap.js`,
+    `${vendorDir}//jquery/dist/jquery.js`
+  ]).pipe(minify({
+    ext: {src: '.js', min: '.min.js'}
+  }))
+    .pipe(gulp.dest(jsDir)));
 
 // Copy javascript to javascript folders
 gulp.task('scripts', () => copy('js'));
 
 // Compile Pug files
 gulp.task('pug', () => {
-    let YOUR_LOCALS = {
-        v: "2.0.0"
-    };
+  let YOUR_LOCALS = {
+    v: "2.0.0"
+  };
 
-    return gulp.src(`${srcDir}/**/*.pug`)
-        .pipe(pug({
-            locals: YOUR_LOCALS
-        }))
-        .pipe(gulp.dest(distDir));
+  return gulp.src(`${srcDir}/**/*.pug`)
+    .pipe(pug({
+      locals: YOUR_LOCALS
+    }))
+    .pipe(gulp.dest(distDir));
 });
 
 // Compile SASS files
 gulp.task('sass', () =>
-    gulp.src(`${srcDir}/**/*.scss`)
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest(cssDir)));
+  gulp.src(`${srcDir}/**/*.scss`)
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest(cssDir)));
 
 
 // By default, do all
