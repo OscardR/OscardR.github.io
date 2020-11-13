742e5fa1750695f70814ed474d9cf7e2cbd01922
Óscar Gómez Alcañiz <oscar.gomez.alcaniz@gmail.com>
2020-11-13 16:57:28 +0100

    Changed styles for gradients on CV / Add new background image on CV / Add 404 page

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -8,16 +8,14 @@ import 'bootstrap/dist/js/bootstrap.min.js'
 
 // Different routes load different CSS files
 export function onRouteUpdate({location}) {
+  require('@css/theme.scss');
+
   switch (location.pathname) {
     case withPrefix("/memoria/"):
       require('@css/memoria.scss');
       break;
     case withPrefix("/cv/"):
       require('@css/cv.scss');
-      // explicitly avoiding break here, so CV page gets also theme CSS
-    case withPrefix("/"):
-    default:
-      require('@css/theme.scss');
       break;
   }
 }
